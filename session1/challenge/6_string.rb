9fe2a4f226f89ecc02228dfc7e64447960d9aa99
Dom <dom.mcdonnell@gmail.com>
2017-04-17 18:45:28 +0100

    task 7 was a head scratcher for sure

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,16 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  if return_odds == true
+    string.chars.select.with_index{|x, i| i.odd?}.join
+  else
+    string.chars.select.with_index{|x, i| i.even?}.join
+  end
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,30 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  #string.scan(/r./i).join.gsub('r','').gsub('R','')
+  # above worked for first test but failed the second
+
+  string.scan(/(?<=r)./i).join
+
+  # didn't fully feel comfortable with the solution provided so after much
+  # effort, head scratching and googling I managed to use the above. It worked!!
+
+  # included the given solution for ref
+  #def pirates_say_arrrrrrrrr(string)
+  #  to_return = ""
+  #  add_next = false
+  #
+  #  string.size.times do |index|
+  #      current_char = string[index]
+  #      to_return << current_char if add_next
+  #      add_next = (current_char == "r" || current_char == "R")
+  #  end
+
+  #  to_return
+#end
+
 end
