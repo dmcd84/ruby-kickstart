9553dd3061506b5b9bc7fadcec9414509f304687
Dom <dom.mcdonnell@gmail.com>
2017-04-23 21:01:40 +0100

    explained solutions, was close on 3

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,26 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(n)
+  to_return = {}
+    1.upto n do |crnt_size|
+      next if crnt_size.even?
+      all = Array.new(crnt_size) {|i| i + 1 }
+      evens = all.select{|i| i.even? }
+      to_return[crnt_size] = evens
+    end
+    to_return
+end
+
+# defined that to_return us a hash
+# identified the range from 1 up to the number given
+# lns 13/14 will select the odd numbers (i.e. skipping even ones by using next)
+# from the range and put them into the hash 'to_return'. The keys have now been assigned.
+# all (ln 15) is the creation of an array in which the even numbers are put back
+# we now have an array populated with all of the numbers from 1 to n.
+# evens uses the 'all' array and selects the values that are even.
+# ln 17 then assigns the array called crnt_size to be equal to evens.
+# i.e. contian only even numbers from 1 to n.
+# This array is the values for the hash.
+# ln 19 then returns the hash containing the keys(ln13/14) and the corresponding
+# values (the array crnt_size created by evens)
--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,16 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+  words = Hash.new { 0 }
+  string.split.each { |word| words[word.downcase] += 1 }
+  words
+end
+
+# words has default value of 0
+# the sting that is input is seperated into indiviual words
+# for each of these words, the block is called.
+# the for each word the word is downcased
+# each time the word is encoutered the words default value is incremented by 1\
+# words is then returned on ln 13 
