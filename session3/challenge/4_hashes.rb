9adb5906e860a7ff2f52b999b4478ade6c0e23f1
Dom <dom.mcdonnell@gmail.com>
2017-04-23 21:06:24 +0100

    so very close but had to peek to get ||=

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,12 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+  to_return = {}
+  str.split.each_with_index { |word, index| to_return[word] ||= index }
+  to_return
+end
+
+# very close with this one - but needed to peek at solution to get the "||="
+# to be able to complete.
